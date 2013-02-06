d95f526387b46c981ce0a68385f789a0e46efed7
Jwashton <William@AshtonFam.org>
2013-02-06 16:14:03 -0500

    First Commit

---
+++ SAUBible/gen/edu/southern/cs/saubible/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.southern.cs.saubible;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SAUBible/src/edu/southern/cs/saubible/HomeScreen.java
@@ -0,0 +1,22 @@
+package edu.southern.cs.saubible;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HomeScreen extends Activity {
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_home_screen);
+  }
+  
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.activity_home_screen, menu);
+    return true;
+  }
+  
+}
