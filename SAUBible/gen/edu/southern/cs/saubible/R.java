26708360d39ba5eb8e0cdbfe2f6032daef0415a9
Isaac <Isaac@216.229.233.69>
2013-02-07 17:43:16 -0500

    Merged demonstration branch with main branch.

--- SAUBible/gen/edu/southern/cs/saubible/R.java
+++ SAUBible/gen/edu/southern/cs/saubible/R.java
@@ -29,6 +29,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int bible_button=0x7f040004;
+        public static final int bible_button_2=0x7f040005;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_bible_navigation=0x7f040003;
