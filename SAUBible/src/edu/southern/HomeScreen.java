dcf55e53b022a0e235329cc01074aec0dae6195b
isaachermens <Isaac.Hermens@gmail.com>
2013-03-28 00:12:06 -0400

    Corrected a small oversight - forgot to clear input field

--- SAUBible/src/edu/southern/HomeScreen.java
+++ SAUBible/src/edu/southern/HomeScreen.java
@@ -141,7 +141,7 @@ public class HomeScreen extends SlidingFragmentActivity {
 		  
 		  // Hide the drawer
 		  getSlidingMenu().toggle();
-		  
+		  input.setText("");
 		  // hide the keyboard
 		  InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
 		  imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
